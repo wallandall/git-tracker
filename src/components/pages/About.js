ff1328262dc201141e2fca951fcde774c34239f8
wallandall <wallandall@gmail.com>
2020-03-03 21:45:56 +0100

    Addaed React router

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About Git-Tracker</h1>
+            <p>This app aearches Github users.</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About;
